3122336c77ee8ef412fa4b18d6e34da2c5206498
albertng <albertng412@gmail.com>
2020-10-10 07:11:18 +0800

    Same server
    Should be work

--- generate-certificate-api/certificate.py
+++ generate-certificate-api/certificate.py
@@ -1,9 +1,16 @@
+import glob
+import os
+import json
+import requests
+import urllib.request
 from datetime import date
 import logging
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
 from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 CORS(app, support_credentials=True) 
@@ -49,6 +56,34 @@ def generate_certificate():
         im.save("certificate_" + i + ".pdf")
 
     return "Generated"
+
+@app.route("/uploadgdrive", methods=['GET'])
+@cross_origin(support_credentials=True)
+def upload_gdrive():
+    try:
+        for file in glob.glob("*.pdf"):
+            # print(file)
+            # put ur access token after the word 'Bearer '
+            headers = {"Authorization": "Bearer "+os.getenv("GDRIVETOKEN")}
+            para = {
+                "name": file,  # file name to be uploaded
+                # make a folder on drive in which you want to upload files; then open that folder; the last thing in present url will be folder id
+                "parents": ["1Vp46IYeMGRkimOFWhfhGUgNJ102yCvx-"]
+            }
+            files = {
+                'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),
+                # replace 'application/zip' by 'image/png' for png images; similarly 'image/jpeg' (also replace your file name)
+                'file': ('application/zip', open(file, "rb"))
+            }
+            r = requests.post(
+                "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+                headers=headers,
+                files=files
+            )
+            print(r.text)
+        return "Upload"+r.text
+    except urllib.error:
+        return urllib.error
 
 #submit()
 if __name__ == '__main__':
