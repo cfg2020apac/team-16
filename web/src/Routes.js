f270fe1f6d368c799bb72082eb43c6ba8442d1b4
Josef Natanael <jnatanael@connect.ust.hk>
2020-10-10 06:30:15 +0800

    Basic Home Component

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -4,7 +4,6 @@ import { AppBar } from "./components/AppBar";
 import { Home } from "./components/Home";
 import { ParticipantStudents } from "./components/ParticipantStudents";
 import CalendarComponent from "./components/CalendarComponent";
-import AnotherCalendarComponent from "./components/AnotherCalendarComponent";
 import { ProjectProgress } from "./components/Progress";
 import { Submission } from "./components/submission"
 import { SubmissionTasks } from "./components/SubmissionTasks"
@@ -19,7 +18,6 @@ export const Routes = () => {
         <AppBar>
           <Route path="/" exact component={Home} />
           <Route path="/calendar" component={CalendarComponent} />
-          <Route path="/anothercalendar" component={AnotherCalendarComponent} />
           <Route exact path="/students/participants" component={ParticipantStudents} />
           <Route exact path="/progress" component={ProjectProgress} />
           <Route exact path="/submissions" component={Submission} />
