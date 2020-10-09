d5852dcca2b4ab7426d383c2db891e19b5cbf2e7
Josef Natanael <jnatanael@connect.ust.hk>
2020-10-09 16:51:09 +0800

    Basic react router

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { Button } from 'antd';
 import "./App.less";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import { AppBar } from "./AppBar";
+import AboutComponent from "./components/AboutComponent"
 
 function App() {
   return (
-    <div className="App">
-      <AppBar/>
-    </div>
+    <Router>
+      <div className="App">
+        <AppBar/>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={AboutComponent} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
 
 export default App;
