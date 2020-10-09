a6785be8a3ef85345393ac6ffe1bb78c3fbb707e
albertng <albertng412@gmail.com>
2020-10-09 23:24:18 +0800

    Update with Joey Cert and modified the code
    Postman

--- generate-certificate-api/certificate.py
+++ generate-certificate-api/certificate.py
@@ -23,23 +23,24 @@ def generate_certificate():
     name_list=new_store["participants"]
     app.logger.info(name_list) 
     for i in name_list:
-        im = Image.open(r'image.jpg')
+        im = Image.open(r'Certificate.jpg')
         d = ImageDraw.Draw(im)
-        location = (275, 181)
+        location = (888, 704)
         text_color = (0, 137, 209)
-        font = ImageFont.truetype("Arial.ttf", 120)
+        font = ImageFont.truetype("AlexBrush-Regular.ttf", 100)
         d.text(location, i, fill = text_color, font = font)
 
         d1 = ImageDraw.Draw(im)
-        location1 = (300, 313)
+        location1 = (1260, 1123)
         text_color1 = (0, 100, 188)
-        font1 = ImageFont.truetype("Arial.ttf", 20)
-        d1.text(location1, "Thank you", fill = text_color1, font = font1)
+        font1 = ImageFont.truetype("Arial.ttf", 50)
+        d1.text(location1, "JA Hong Kong", fill = text_color1, font = font1)
+
 
         d2 = ImageDraw.Draw(im)
-        location2 = (300, 270)
+        location2 = (383, 1125)
         text_color2 = (0, 100, 90)
-        font2 = ImageFont.truetype("Arial.ttf", 20)
+        font2 = ImageFont.truetype("Arial.ttf", 50)
         d2.text(location2, dayform, fill = text_color2, font = font2)
 
         im.save("certificate_" + i + ".pdf")
