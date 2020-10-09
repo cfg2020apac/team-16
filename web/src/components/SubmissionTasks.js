91e4a30f76de2f5a587c30262b8880c0c30dd9c5
agarwalmanya <manya18ag@gmail.com>
2020-10-10 03:41:48 +0800

    Submissions optimisations

--- web/src/components/SubmissionTasks.js
+++ web/src/components/SubmissionTasks.js
@@ -4,27 +4,23 @@ import { FirebaseDB } from "../firebase";
 import { Link } from "react-router-dom";
 
 const gridStyle = {
-    width: '25%',
-    textAlign: 'center',
-  };
+  width: "25%",
+  textAlign: "center",
+};
+
+const data = ["Task 1", "Task 2", "Task 3", "Task 4"];
 // TODO: In the below card, there should be a way to receive parameter from previous link to set the name
-export const SubmissionTasks = () => {
-    return (
+export const SubmissionTasks = ({ location }) => {
+  const { title } = location.state;
+  return (
     <div className="site-card-wrapper">
-    <Card title="Program Name" noHovering>
-        <Card.Grid style={gridStyle}>
-        <Link to="/viewSubmissions">Task1</Link>
-        </Card.Grid>
-        <Card.Grid style={gridStyle}>
-        <Link to="/viewSubmissions">Task2</Link>
-        </Card.Grid>
-        <Card.Grid style={gridStyle}>
-        <Link to="/viewSubmissions">Task3</Link>
-        </Card.Grid>
-        <Card.Grid style={gridStyle}>
-        <Link to="/viewSubmissions">Task4</Link>
-        </Card.Grid>
-    </Card>
+      <Card title={title} noHovering>
+        {data.map((task) => (
+          <Link to="/viewSubmissions">
+            <Card.Grid style={gridStyle}>{task}</Card.Grid>
+          </Link>
+        ))}
+      </Card>
     </div>
-    );
+  );
 };
