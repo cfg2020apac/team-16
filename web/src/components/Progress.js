348806b7058d2e7324c08062c44d08c488fc1cb0
agarwalmanya <manya18ag@gmail.com>
2020-10-10 00:06:32 +0800

    generate certificate api cors

--- web/src/components/Progress.js
+++ web/src/components/Progress.js
@@ -1,9 +1,35 @@
 import React from "react"
+import { Collapse } from 'antd';
+
+const { Panel } = Collapse;
+
+function callback(key) {
+  console.log(key);
+}
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+const data = [
+    { 
+        title: "Planning with Purpose"
+    }, 
+]
 
 export const ProjectProgress = () => { 
+    
     return(
-        <div>
-            
-        </div>
+        <Collapse defaultActiveKey={['1']} onChange={callback}>
+        {
+            data.map(project =>  
+                <Panel showArrow={false} header={project.title} key="1">
+                    <p>{text}</p>
+                </Panel>
+            )
+        } 
+        </Collapse>
     )
 }
